a4d44c709aebd0c5b2d74398f8810912419db0d0
Mateusz <mateuszdygas114@gmail.com>
2021-06-27 23:38:40 +0200

    Add unit tests - refactor

--- src/test/java/rcrscopy/CopierTest.java
+++ src/test/java/rcrscopy/CopierTest.java
@@ -16,25 +16,8 @@ import rcrscopy.scanners.RecursiveFileScanner;
 
 class CopierTest {
 
-	Path path1, path2;
-    File file1, file2;
-	
-    /* This directory and the files created in it will be deleted after
-     * tests are run, even in the event of failures or exceptions.
-     */
-    @TempDir
-    Path tempDir, tempDir1;
-
 	@Test
 	void copyFilesTest() throws Exception {
-		path1 = tempDir.resolve( "testfile1.txt" );
-        path2 = tempDir.resolve( "testfile2.txt" );
-        file1 = path1.toFile();
-        file2 = path2.toFile();
-        
-		ArgumentsParser parser = new ArgumentsParser(new String[] {tempDir.toString(), tempDir1.toString(), "5"} );
-		ArgumentsConfig config =  parser.parse();
-		Copier copier = new Copier(new RecursiveFileScanner());
-	}
 
+	}
 }
